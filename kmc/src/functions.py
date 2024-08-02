c08f841b3cb2083d28bbc6cf8ab0475651c4e1d0
Tuzass <artur.costa2001@gmail.com>
2024-08-02 17:40:55 -0300

    feat: both kmc algorithms implemented

--- kmc/src/functions.py
+++ kmc/src/functions.py
@@ -1,7 +1,23 @@
 import numpy as np
+import random
 
 # constant for Chebyshev distance
 K_CHEBYSHEV = np.inf
+
+# decimal places in output
+K_DECIMAL_PLACES = 3
+
+# k parameter for the k-means clustering algorithms
+K_PARAMETER = 5
+
+# number of tests performed for each algorithm
+K_NUMBER_OF_TESTS = 30
+
+# depth of recursion in the binary search version of the algorithm
+K_BINARY_SEARCH_DEPTH = 10
+
+# enables and disables output of tests
+K_TEST_OUTPUT_ENABLED = False
 
 # reads points from file and returns a list of them in (x, y) format
 def readPoints(file):
@@ -87,3 +103,116 @@ def distanceTable(points, p):
         table.append(row)
 
     return table
+
+# returns the longest distance in the given table
+def getMaxDistance(table):
+    longest = - np.inf
+
+    for row in table:
+        longest_in_row = max(row)
+        if longest_in_row > longest:
+            longest = longest_in_row
+
+    return longest
+
+# returns the greatest distance from a point to its closest center
+def getMaxRadius(centers, table):
+    max_distance = - np.inf
+    number_of_points = len(table)
+    points = list(range(number_of_points))
+
+    for i in points:
+        closest_distance = np.inf
+
+        for j in centers:
+            distance = table[i][j]
+            if distance < closest_distance:
+                closest_distance = distance
+        
+        if closest_distance > max_distance:
+            max_distance = closest_distance
+
+    return max_distance
+
+# returns the set of centers
+# uses the algorithm that chooses the furthest point to continue
+def furthestFirstKMC(table, k=K_PARAMETER):
+    number_of_points = len(table)
+    points = list(range(number_of_points))
+
+    if k >= number_of_points:
+        return points
+
+    first_point = random.choice(points)
+    points.remove(first_point)
+    centers = [first_point]
+
+    while len(centers) < k:
+        furthest = -1
+        furthest_distance = - np.inf
+        
+        for i in points:
+            closest_distance = np.inf
+            for j in centers:
+                distance = table[i][j]
+                if distance < closest_distance:
+                    closest_distance = distance
+            if closest_distance > furthest_distance:
+                furthest_distance = closest_distance
+                furthest = i
+        
+        centers.append(furthest)
+        points.remove(furthest)
+    
+    return centers
+
+# returns the set of centers
+# uses the algorithm that performs a binary search in the interval [lower_bound, upper_bound]
+# to use log_file, just open 'log.txt' on main.py and pass it as an argument here
+def binarySearchKMC(table, lower_bound, upper_bound, depth=K_BINARY_SEARCH_DEPTH, k=K_PARAMETER):
+    # log_file.write(f"\n\n\nlower_bound = {round(lower_bound, K_DECIMAL_PLACES)}, upper_bound = {round(upper_bound, K_DECIMAL_PLACES)}, depth = {depth}")
+    centers = []
+    number_of_points = len(table)
+    points = list(range(number_of_points))
+    mid_point = lower_bound + (upper_bound - lower_bound) / 2
+    # log_file.write(f"\nmid_point = {round(mid_point, K_DECIMAL_PLACES)}")
+
+    while len(points) > 0:
+        c = random.choice(points)
+        centers.append(c)
+        remaining = points.copy()
+
+        for i in points:
+            distance = table[c][i]
+            if distance <= mid_point:
+                remaining.remove(i)
+        
+        points = remaining.copy()
+
+    if len(centers) <= k:
+        # log_file.write(f"\nsuccess! centers = {centers}")
+        if depth == 0:
+            # log_file.write(f"\nreturning centers {centers}")
+            return centers
+        
+        # log_file.write(f"\nbeginning search on lower half")
+        lower_half = binarySearchKMC(table, lower_bound, mid_point, depth - 1, k)
+        if lower_half != [None]:
+            return lower_half
+        
+        # log_file.write(f"\n\n\nlower half did not produce results, returning centers {centers}")
+        return centers
+        
+    else:
+        # log_file.write(f"\nfailure! centers = {centers}")
+        if depth == 0:
+            # log_file.write("\nreturning [None]")
+            return [None]
+        
+        # log_file.write(f"\nbeginning search on upper half")
+        upper_half = binarySearchKMC(table, mid_point, upper_bound, depth - 1, k)
+        if upper_half != [None]:
+            return upper_half
+
+    # log_file.write("\n\n\nupper half did not produce results, returning [None]")
+    return [None]
--- kmc/src/main.py
+++ kmc/src/main.py
@@ -5,7 +5,6 @@ points = []
 with open("points.txt") as points_file:
     points = readPoints(points_file)
 
-"""
 p1_distance_table = distanceTable(points, 1)
 p2_distance_table = distanceTable(points, 2)
 chebyshev_distance_table = distanceTable(points, K_CHEBYSHEV)
@@ -18,4 +17,33 @@ printTable(p2_distance_table)
 
 print ("\n\nChebyshev distance table:")
 printTable(chebyshev_distance_table)
-"""
+
+ff_max_radii = []
+print ("\n\n" + "-" * 80 + "\n\n\nFurthest-first k-means clustering")
+print (f"k = {K_PARAMETER}")
+
+for i in range(K_NUMBER_OF_TESTS):
+    centers = furthestFirstKMC(p2_distance_table)
+    max_radius = getMaxRadius(centers, p2_distance_table)
+    ff_max_radii.append(max_radius)
+    if K_TEST_OUTPUT_ENABLED:
+        print (f"\n\nTest {i + 1}\nCenters: {centers}\nMax radius: {round(max_radius, K_DECIMAL_PLACES)}")
+
+average = round(sum(ff_max_radii) / K_NUMBER_OF_TESTS, K_DECIMAL_PLACES)
+print (f"\n\nNumber of tests: {K_NUMBER_OF_TESTS}\nAverage max radius: {average}")
+
+bs_max_radii = []
+max_distance = getMaxDistance(p2_distance_table)
+print ("\n\n" + "-" * 80 + "\n\n\nBinary-search k-means clustering")
+print (f"k = {K_PARAMETER}, binary search depth = {K_BINARY_SEARCH_DEPTH}")
+
+for i in range(K_NUMBER_OF_TESTS):
+    centers = binarySearchKMC(p2_distance_table, 0, max_distance)
+    max_radius = getMaxRadius(centers, p2_distance_table)
+    bs_max_radii.append(max_radius)
+    if K_TEST_OUTPUT_ENABLED:
+        print (f"\n\nTest {i + 1}\nCenters: {centers}\nMax radius: {round(max_radius, K_DECIMAL_PLACES)}")
+
+average = round(sum(bs_max_radii) / K_NUMBER_OF_TESTS, K_DECIMAL_PLACES)
+print (f"\n\nNumber of tests: {K_NUMBER_OF_TESTS}\nAverage max radius: {average}")
+print ("\n\n" + "-" * 80 + "\n\n")
